646af5d2f711f79a86df3567c4a1819f205f9767
kristijonas <kristijonas.zibutis@gmail.com>
2016-08-16 17:32:36 +0200

    90% done with project_skills, projects and skills. We are left with permission now

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ApplicationRecord
-  has_many :project_skills
+  has_many :project_skills, inverse_of: :project, dependent: :destroy
   has_many :applications
   has_many :users, through: :applications
   has_many :skills, through: :project_skills
   belongs_to :author, class_name: User
+  accepts_nested_attributes_for :project_skills
 end
