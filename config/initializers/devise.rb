0213b844078707ce9ffc0e4cde68909930520196
Nathan Kuik <nathan.kuik@gmail.com>
2016-08-16 13:52:32 +0200

    Facebook authentication in development added and ready to test with navbar

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,6 +7,7 @@ Devise.setup do |config|
   info_fields: 'email, first_name, last_name',
   image_size: 'square',  # 50x50, guaranteed ratio
   secure_image_url: true
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
