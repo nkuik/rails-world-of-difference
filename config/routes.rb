3f4cf0c7b92c199334a4b068cac400f1bea65ce7
Nathan Kuik <nathan.kuik@gmail.com>
2016-08-18 14:59:18 +0200

    Create selected projects view, make adjustments to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root to: 'pages#home'
 
+  get "projects/selected", to: "projects#selected"
+
   resources :projects do
     resources :applications
   end
   resources :skill_users, only: [:new, :create, :destroy]
   resources :profiles
-
 
 end
